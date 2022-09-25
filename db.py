fa0ea3ba373c46aa338742802a975fb09838045c
mahikap <mp2243@cornell.edu>
2022-09-25 17:01:41 -0400

    Added locks and polished code

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
-# record = {'127.0.0.1:32689':[2345627354, 6]}
-
-
+import random
+import socket
 
 def addServer(record: dict, tcpIP: str, timeStamp: int, digit: int):
     """
@@ -55,7 +54,26 @@ def convertRecordToString(record: dict):
     return final #final is ['127.0.0.1:56789,12345,5']
 
 
+def selectRandomPort(record, PORT):
+    '''
+    Returns a random port number from record. Ensures that it does not return the system's local port. 
+    This function is called every 3 seconds
+    '''
+    #choose from the keys in the dict, ie. this is a list of record keys (IP/port pair) --> string var '192.68.255.255:26789'
 
+    randomTCPIP = random.choice(list(record))   
+    
+    host_name  = socket.gethostname()
+    host_ip = socket.gethostbyname(host_name)
 
+    myTCPIP = host_ip+':'+str(PORT)
+
+    #if the randomizer selected myTCPIP, then run it again
+    print('available records: ', record)
+    while randomTCPIP == myTCPIP:  #compare string to string
+        randomTCPIP = random.choice(list(record)) 
+    
+    tcpipToGossipTo = randomTCPIP.split(':') # e.g. [127.0.0.1, 50000]
+    return tcpipToGossipTo #prints a list of strings with an IP, port format [127.0.0.1, 50000]
 
 
--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-#Accept input from console for digits
-#Connect to clients every 3 sec 
-#Respond when clients connect 
-#This part of the program will also include the threading
-
 import threading
 import socket
 import random
@@ -13,32 +8,7 @@ import sys
 import db
 
 
-# put in db.py
-# use lock anytime record is accessed
-def selectRandomPort(record, PORT):
-    '''
-    Returns a random port number from record. Ensures that it does not return the system's local port. 
-    This function is called every 3 seconds
-    '''
-    #choose from the keys in the dict, ie. this is a list of record keys (IP/port pair) --> string var '192.68.255.255:26789'
-
-    randomTCPIP = random.choice(list(record))   
-    
-    host_name  = socket.gethostname()
-    host_ip = socket.gethostbyname(host_name)
-
-    myTCPIP = host_ip+':'+str(PORT)
-
-    #if the randomizer selected myTCPIP, then run it again
-    print('available records: ', record)
-    while randomTCPIP == myTCPIP:  #compare string to string
-        randomTCPIP = random.choice(list(record)) 
-    
-    tcpipToGossipTo = randomTCPIP.split(':') # e.g. [127.0.0.1, 50000]
-    return tcpipToGossipTo #prints a list of strings with an IP, port format [127.0.0.1, 50000]
-
-
-def server(event):
+def server(event, lock):
     '''
     Recieves data from other clients and updates record
     '''
@@ -55,10 +25,8 @@ def server(event):
                     print(f"connected to {addr}")  
                     s.setblocking(True)
             
-                    # while True:
-                    print_lock.acquire()
+                    # with lock:
                     recordList = db.convertRecordToString(record)
-                    print_lock.release()
                     for r in recordList:
                         conn.sendall(r.encode())
                         # response = conn.recv(2048)
@@ -68,7 +36,7 @@ def server(event):
                 
 
 
-def client(event):
+def client(event, lock):
     '''
     Accesses record, selects ports to gossip to, and sends data
     '''
@@ -83,7 +51,9 @@ def client(event):
         # instantiate a socket
         y = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
-        random_server = selectRandomPort(record, PORT)
+        with lock:
+            random_server = db.selectRandomPort(record, PORT)
+        
         print(f"Connecting to a random port: {random_server}")
         y.connect((random_server[0], int(random_server[1])))
 
@@ -93,14 +63,15 @@ def client(event):
             if data:
                 data = data.decode(encoding='utf-8', errors='strict') #return string
                 data = data.split(",") #return list based on split values
-                print_lock.acquire()
-                if data[0] in record.keys():
-                    db.updateServer(record, data[0], int(data[1]), int(data[2]))
-                else:
-                    db.addServer(record, data[0], int(data[1]), int(data[2]))
-                print_lock.release()
-                print("TCP: ", data)
+                with lock:
+                    print(f"Data received from server: {data}")
+                    if data[0] in record.keys():
+                        db.updateServer(record, data[0], int(data[1]), int(data[2]))
+                    else:
+                        db.addServer(record, data[0], int(data[1]), int(data[2]))
+                
             data = y.recv(1024)
+
             
         y.close()
 
@@ -114,7 +85,7 @@ def client(event):
 
 if __name__ == "__main__":
 
-    print_lock = threading.Lock()
+    lock = threading.Lock()
     ipAddress = sys.argv[1] #server inputs its own ip addr / client inputs ip address it wants to connect to
     PORT = int(sys.argv[2])
     
@@ -128,8 +99,8 @@ if __name__ == "__main__":
     client_event = threading.Event() #False
     
     # creating threads
-    serverThread = threading.Thread(target=server, name='serverThread', args=(server_event,))
-    clientThread = threading.Thread(target=client, name='clientThread', args=(client_event,))
+    serverThread = threading.Thread(target=server, name='serverThread', args=(server_event,lock,))
+    clientThread = threading.Thread(target=client, name='clientThread', args=(client_event,lock,))
 
     # starting threads
     serverThread.start()
@@ -147,6 +118,7 @@ if __name__ == "__main__":
                 db.addServer(record, d[1:], 0, 0)
             elif int(d) in range (0,10):
                 print("updating your own digit")
+                # with lock:
                 db.updateServer(record, ipAddress+':'+str(PORT), int(time.time()), d)
                 print(record)      
 
